c703b0945299abcaf1598e632612d2b67ca7b7b9
Said-Abdikarim <kareemfrmto@gmail.com>
2019-07-17 20:36:19 -0400

    USING JSONPLACEHOLDER API: GET/POST/PUT/DELETE
    USING JSONPLACEHOLDER API: GET/POST/PUT/DELETE

--- Lesson 5 - Custom HTTP library with AJAX CALLBACKS/app.js
+++ Lesson 5 - Custom HTTP library with AJAX CALLBACKS/app.js
@@ -1,10 +1,43 @@
 
 //Instantiate the object
 const jsonph = new JSONPH();
-jsonph.get('https://jsonplaceholder.typicode.com/posts', function (error, item) {
-  if (error) { //The string is a truthy value, whereas null and undefined are not.
-    console.log(error)
-  } else {
-    console.log(item);
+
+//GET
+// jsonph.get('https://jsonplaceholder.typicode.com/posts/1', function (error, item) {
+//   if (error) { //The string is a truthy value, whereas null and undefined are not.
+//     console.log(error)
+//   } else {
+//     console.log(item);
+//   }
+// })
+
+//Create post that we want to update
+const data = {
+  title: 'Lion King',
+  body: 'The Live Version 2019'
+};
+
+
+//POST
+// jsonph.post('https://jsonplaceholder.typicode.com/posts', data, function (post) {
+//   console.log(post);
+// })
+
+
+//PUT
+// jsonph.put('https://jsonplaceholder.typicode.com/posts/1', data, function (err, post) {
+//   if (err) { //The string is a truthy value, whereas null and undefined are not.
+//     console.log(err)
+//   } else {
+//     console.log(post);
+//   }
+// })
+
+//DELETE
+jsonph.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response){
+  if(err){
+    console.log(err);
+  }else{
+    console.log(response);
   }
 })
--- Lesson 5 - Custom HTTP library with AJAX CALLBACKS/jsonph.js
+++ Lesson 5 - Custom HTTP library with AJAX CALLBACKS/jsonph.js
@@ -19,13 +19,70 @@ JSONPH.prototype.get = function (url, callback) {
         }
     }
 
-
     this.http.send();
 }
 
-//Make HTTP GET Request
+//Make HTTP POST Request
 
-//Make HTTP GET Request
+JSONPH.prototype.post = function (url, data, callback) {
 
-//Make HTTP GET Request
+    let self = this; //capture the this reference in this scope.
+
+    //No need to check status, since this is a POST request.
+    this.http.onload = function () {
+        callback(null, self.http.responseText);
+    }
+
+    //open the connection
+    this.http.open('POST', url, true);
+
+    //Designates the content to be in JSON format
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+
+    //Since we are passing the data or adding to it, we need to convert our object to JSON.
+    this.http.send(JSON.stringify(data));
+
+}
+
+//Make HTTP PUT Request
+
+JSONPH.prototype.put = function (url, data, callback) {
+
+    let self = this; //capture the this reference in this scope.
+
+    //No need to check status, since this is a POST request.
+    this.http.onload = function () {
+        callback(null, self.http.responseText);
+    }
+
+    //open the connection
+    this.http.open('PUT', url, true);
+
+    //Designates the content to be in JSON format
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+
+    //Since we are passing the data or adding to it, we need to convert our object to JSON.
+    this.http.send(JSON.stringify(data));
+
+}
+
+
+//Make HTTP DELETE Request
+
+JSONPH.prototype.delete = function (url, callback) {
+    this.http.open('DELETE', url, true);
+
+    let self = this; //capture the this reference in this scope.
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            callback(null, "POST DELETED");
+        } else {
+            callback("Error: " + self.http.status);
+        }
+    }
+
+    this.http.send();
+}
 
